4ad96dea342fab329e2578fc9fea8f5a1d7174db
kenneth ramsey <kramsey@coretechs.net>
2017-07-21 02:11:45 -0400

    change btn class from default to primary

--- BugTracker/Models/BugTrackerModels.Designer.cs
+++ BugTracker/Models/BugTrackerModels.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Darwin\Documents\Visual Studio 2013\Projects\BugTracker\BugTracker\Models\BugTrackerModels.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Code\Personal\BugTracker\BugTracker\Models\BugTrackerModels.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
